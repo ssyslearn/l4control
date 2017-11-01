141feb17af6ad12903792eb0813ed4096bfc7008
Seungmin Lim <seungmin.lim@sk.com>
2017-11-01 19:23:44 +0900

    로그인 기능 추가하였으나 홈으로 돌아오지 않음

--- l4control/urls.py
+++ l4control/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import *
-from django.contrib.auth import views as auth_views
-from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +10,10 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('l4ui.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^logout/$', auth_views.logout, {'next_page' : '/'}),
-    url(r'^login/$', auth_views.login,  {'template_name':'registration/login.html'}),
+    #url(r'^accounts/', include('django.contrib.auth.urls'))
+    # url(r'^accounts/', include('django.contrib.auth.urls')),
+    #url(r'^logout/$', auth_views.logout, {'next_page' : '/'}),
+    #url(r'^login/$', auth_views.login,  {'template_name':'registration/login.html'}),
+    url(r'^login/$', django.contrib.auth.views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/$', django.contrib.auth.views.logout, {'next_page': '/'}, name='logout'),
 ]
