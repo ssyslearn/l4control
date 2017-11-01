1e3ed1eb49b87aa9e4f7c4124f4b834f3a51798a
Seungmin Lim <seungmin.lim@sk.com>
2017-11-01 15:09:32 +0900

    django 1.11.2는 patterns 함수 사용하지 않음

--- l4control/urls.py
+++ l4control/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'l4control.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('l4ui.urls')),
-)
+]
