1acdd572e34cd4d0be95e4bc645cf5889b0d03c0
Seungmin Lim <seungmin.lim@sk.com>
2017-11-07 21:06:14 +0900

    components props 기능 추가

--- l4ui/templates/l4ui/create_l4.vue
+++ l4ui/templates/l4ui/create_l4.vue
@@ -25,101 +25,32 @@
                 <button v-on:click="assign" class="btn btn-lg btn-info" >IP 할당</button>
             </div>
         </div>
-        
-        <div id="service_name">
-            <div class="col-md-12">
-                <label class="label-title">서비스 명</label>
-            </div>
-            <div class="col-md-8">
-                <input v-model="input_service_name" type="text"  placeholder="서비스명을 입력하세요" required="" class="form-control input-lg" >
-                <p> ${ input_service_name } </p>
-            </div>
-            <div class="col-md-4">
-                <!--
-                <label class="label-title">v_${ srv_input_vip }_${ input_service_name }_port</label>
-                -->
-            </div>
-            <div class="col-md-12"><br></div>
-        </div>
 
 
-        <div class="col-md-12">
-            <label class="label-title" id="label_virtual_port">Virtual Server</label>
-            <button class="btn btn-xs btn-info">+</button>
-            <div class="col-md-round-box">
-                <div class="col-md-4">
-                    <div class="col-md-12">
-                        <label class="label-title" id="label_virtual_port">Virtual Port</label>
-                    </div>
-                    <div class="col-md-round-box">
-                        <div class="col-md-12-inner">
-                            <label class="label-subtitle">Port</label>
-                            <input type="text" id="rip" placeholder="ex. 80" required="" class="form-control input-lg">
-                        </div>
-                        <div class="col-md-12-inner">
-                            <label class="label-subtitle">Sticky 사용 여부</label>
-                            <select class="form-control input-md" id="sel1">
-                                <option>사용 안함</option>
-                                <option>300초</option>
-                                <option>600초</option>
-                                <option>900초</option>
-                                <option>1800초</option>
-                            </select>
-                        </div>
-
-                        <div class="col-md-12-inner">
-                            <label class="label-subtitle">DSR 사용 여부</label>
-                            <select class="form-control input-md" id="sel1">
-                                <option>사용 안함</option>
-                                <option>사용</option>
-                            </select>
-                        </div>
-                        <div class="col-md-12-inner">
-                            <label class="label-subtitle">SSL 인증서 사용 여부 </label>
-                            <select class="form-control input-md" id="sel1">
-                                <option>사용 안함</option>
-                                <option>사용</option>
-                            </select>
-                        </div>
-                    </div>
+            <div id="service_name">
+                <div class="col-md-12">
+                    <label class="label-title">서비스 명</label>
                 </div>
                 <div class="col-md-8">
-                    <div class="col-md-12">
-                        <label class="label-title" id="label_real_server">Real Server</label>
-                        <button class="btn btn-xs btn-info">+</button>
-                    </div>
-                    <div class="col-md-round-box">
+                    <input v-model="input_service_name" type="text"  placeholder="서비스명을 입력하세요" required="" class="form-control input-lg" >
+                    <!--<span class="label-svctitle" v-for="index in vs_count" v-model="virtual_port[0]">-->
+                    <span class="label-svctitle" v-for="index in vs_count">
+                        <!--v_${ input_vip }_${ input_service_name }_${ port },-->
+                        v_${ input_vip }_${ input_service_name }_${ virtual_port[index] },
+                    </span>
+                </div>
+                <div class="col-md-12"><br></div>
+            </div>
 
-                        <div class="col-md-4">
-                            <label class="label-subtitle">Real IP</label>
-                            <input type="text" id="sticky" placeholder="ex. 10.10.1.1" required="" class="form-control input-md">
-                        </div>
-                        <div class="col-md-2">
-                            <label class="label-subtitle">Port</label>
-                            <input type="text" id="sticky" placeholder="ex. 80" required="" class="form-control input-md">
-                        </div>
-                        <div class="col-md-2">
-                            <label class="label-subtitle">LB mode</label>
-                            <select class="form-control input-md" id="sel1">
-                                <option>Round Robin</option>
-                                <option>Least Connections</option>
-                            </select>
-                        </div>
-                        <div class="col-md-3">
-                            <label class="label-subtitle">Monitor</label>
-                            <select class="form-control input-md" id="sel1">
-                                <option>http_healthcheck.jsp_skphok</option>
-                                <option>tcp</option>
-                                <option>tcp_half_open</option>
-                                <option>icmp</option>
-                                <option>udp</option>
-                            </select>
-                        </div>
-
-                    </div>
+            <div id="virtual_server" class="col-md-12">
+                <label class="label-title">Virtual Server</label>
+                <button v-on:click="add_vs" class="btn btn-xs btn-info">+</button>
+                <div v-bind:id="`vs-box-${ index }`" v-for="index in vs_count">
+                <!--<div v-bind:id="`vs-box-${ vport }`" v-for="vport in virtual_port">-->
+                    <virtual-server-box></virtual-server-box>
                 </div>
             </div>
-        </div>
+
 
 
         <div class="col-md-12" align="right">
@@ -134,10 +65,6 @@
 
 </div>
 
-
-<input type="text" v-model="test" placeholder="Virtual IP" required="" class="form-control input-lg">
-<p v-text="test"></p>
-
 <script src="/static/js/jquery/1.11.3/jquery.min.js"></script>
 <script src="/static/js/jquery.redirect.js"></script>
 <link href="/static/css/l4style.css" rel="stylesheet" type="text/css">
@@ -148,15 +75,18 @@
 
     Vue.options.delimiters = ['${', '}'];
 
+    var data = {
+        vs_count: 1,
+        input_vip: '',
+        input_service_name: '',
+        virtual_port: []
+    }
+
     var assign_vip = new Vue({
         el: '#assign_vip',
-        data: {
-            input_vip: ''
-        },
-        // 메소드는 `methods` 객체 안에 정의합니다
+        data: data,
         methods: {
             assign: function (event) {
-                // 메소드 안에서 사용하는 `this` 는 Vue 인스턴스를 가리킵니다
                 alert('Hello ' + this.input_vip + '!')
             }
         }
@@ -164,11 +94,95 @@
 
     var service_name = new Vue({
         el: '#service_name',
-        data: {
-            input_service_name: ''
+        data: data
+    })
+
+
+    var virtual_server_box = {
+            props: ['virtual_port'],
+            data: function() {
+                return {
+                    vs_box_port: this.virtual_port
+                }
+            },
+            template: '<div class="col-md-round-box">' +
+            '<div class="col-md-4"> ' +
+            '<div class="col-md-12"><label class="label-title">Virtual Port</label></div> ' +
+            '<div class="col-md-round-box">' +
+            '<div class="col-md-12-inner" >' +
+            '<label class="label-subtitle">Port</label> ' +
+            '<input type="text" placeholder="ex. 80" required="" class="form-control input-lg" v-model="vs_box_port">' +
+            '</div> ' +
+            '<div class="col-md-12-inner">' +
+            '<label class="label-subtitle">Sticky 사용 여부</label><select class="form-control input-md" id="sel1"> ' +
+            '<option>사용 안함</option><option>300초</option> <option>600초</option> <option>900초</option> <option>1800초</option> ' +
+            '</select>' +
+            '</div>' +
+            '<div class="col-md-12-inner"><label class="label-subtitle">DSR 사용 여부</label> ' +
+            '<select class="form-control input-md" id="sel1"> <option>사용 안함</option> <option>사용</option> </select>' +
+            '</div> ' +
+            '<div class="col-md-12-inner">' +
+            '<label class="label-subtitle">SSL 인증서 사용 여부 </label><select class="form-control input-md" id="sel1"> ' +
+            '<option>사용 안함</option> <option>사용</option> </select>' +
+            '</div>' +
+            '</div>' +
+            '</div> ' +
+            '<div class="col-md-8">' +
+            '<div class="col-md-12">' +
+            '<label class="label-title" id="label_real_server">Real Server</label> ' +
+            '<button class="btn btn-xs btn-info">+</button>' +
+            '</div>' +
+            '<div class="col-md-round-box"> ' +
+            '<div class="col-md-4">' +
+            '<label class="label-subtitle">Real IP</label> ' +
+            '<input type="text" id="sticky" placeholder="ex. 10.10.1.1" required="" class="form-control input-md">' +
+            '</div> ' +
+            '<div class="col-md-2">' +
+            '<label class="label-subtitle">Port</label><input type="text" id="sticky" placeholder="ex. 80" required="" class="form-control input-md"> ' +
+            '</div>' +
+            '<div class="col-md-2">' +
+            '<label class="label-subtitle">LB mode</label> <select class="form-control input-md" id="sel1"> ' +
+            '<option>Round Robin</option> <option>Least Connections</option> </select>' +
+            '</div> ' +
+            '<div class="col-md-3">' +
+            '<label class="label-subtitle">Monitor</label> <select class="form-control input-md" id="sel1"> ' +
+            '<option>http_healthcheck.jsp_skphok</option> <option>tcp</option> <option>tcp_half_open</option> <option>icmp</option> ' +
+            '<option>udp</option> </select>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>',
+
+    }
+
+    var virtual_server = new Vue({
+        el: '#virtual_server',
+        data: data,
+        components: {
+            'virtual-server-box' : virtual_server_box
+        },
+        methods: {
+            add_vs: function (event) {
+                if (this.vs_count < 5) {
+                    this.vs_count += 1;
+                    //alert(this.$el.getElementsByClassName("vport_class")[this.vs_count-1].text);
+                    //this.virtual_port.push(this.$el.getElementsByClassName("vport_class")[this.vs_count-1].value);
+                    //this.virtual_port = assign_vport.virtual_port;
+                    this.virtual_port = this.$children;
+                }
+            }
         }
     })
 
+    var assign_vport = new Vue({
+        el: '#assign_vport',
+        data: data,
+        methods: {
+            changed: function(){
+                alert(10);
+            }
+        }
+    })
 
 </script>
 
--- l4ui/templates/l4ui/test.vue
+++ l4ui/templates/l4ui/test.vue
@@ -1,37 +1,149 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-      <script src="https://unpkg.com/vue"></script>
-  <title>JS Bin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <script src="https://unpkg.com/vue"></script>
+    <title>JS Bin</title>
 </head>
 <body>
 
-  <div id="app">
-   <p v-text="message"></p>
+<div id="app">
+    <p v-text="message"></p>
 </div>
 
-  <div id="app2">
-   <p v-text="message2"></p>
+<div id="app2">
+    <p v-text="message2"></p>
 </div>
 
+
+<div id="vue-instance">
+    <input v-model='items'/>
+    <span v-for="item in items">
+        ${ item }
+    </span>
+</div>
+
+<div id="another-instance">
+    <span v-for="item in items">
+        ${ item }
+    </span>
+</div>
+
+
+<!-- 백업 !!! -->
+
+<!--
+<div id="virtual_server_box" class="col-md-round-box">
+              <div id="assign_vport" class="col-md-4">
+              <div class="col-md-12">
+              <label class="label-title" id="label_virtual_port">Virtual Port</label>
+          </div>
+          <div class="col-md-round-box">
+          <div class="col-md-12-inner">
+          <label class="label-subtitle">Port</label>
+          <input v-model="input_vport" type="text" id="rip" placeholder="ex. 80" required="" class="form-control input-lg">
+          </div>
+          <div class="col-md-12-inner">
+          <label class="label-subtitle">Sticky 사용 여부</label>
+      <select class="form-control input-md" id="sel1">
+          <option>사용 안함</option>
+      <option>300초</option>
+      <option>600초</option>
+      <option>900초</option>
+      <option>1800초</option>
+      </select>
+      </div>
+
+      <div class="col-md-12-inner">
+          <label class="label-subtitle">DSR 사용 여부</label>
+      <select class="form-control input-md" id="sel1">
+          <option>사용 안함</option>
+      <option>사용</option>
+      </select>
+      </div>
+      <div class="col-md-12-inner">
+          <label class="label-subtitle">SSL 인증서 사용 여부 </label>
+      <select class="form-control input-md" id="sel1">
+          <option>사용 안함</option>
+      <option>사용</option>
+      </select>
+      </div>
+      </div>
+      </div>
+
+      <div class="col-md-8">
+          <div class="col-md-12">
+          <label class="label-title" id="label_real_server">Real Server</label>
+      <button class="btn btn-xs btn-info">+</button>
+          </div>
+          <div class="col-md-round-box">
+
+          <div class="col-md-4">
+          <label class="label-subtitle">Real IP</label>
+      <input type="text" id="sticky" placeholder="ex. 10.10.1.1" required="" class="form-control input-md">
+          </div>
+          <div class="col-md-2">
+          <label class="label-subtitle">Port</label>
+          <input type="text" id="sticky" placeholder="ex. 80" required="" class="form-control input-md">
+          </div>
+          <div class="col-md-2">
+          <label class="label-subtitle">LB mode</label>
+      <select class="form-control input-md" id="sel1">
+          <option>Round Robin</option>
+      <option>Least Connections</option>
+      </select>
+      </div>
+      <div class="col-md-3">
+          <label class="label-subtitle">Monitor</label>
+          <select class="form-control input-md" id="sel1">
+          <option>http_healthcheck.jsp_skphok</option>
+          <option>tcp</option>
+          <option>tcp_half_open</option>
+          <option>icmp</option>
+          <option>udp</option>
+          </select>
+          </div>
+
+          </div>
+          </div>
+      </div>
+
+      -->
 
 
 <script>
+
+    Vue.options.delimiters = ['${', '}'];
+
+    var data = {
+        items: []
+    }
+
     var app = new Vue({
-  el: '#app',
-  data: {
-    message: '안녕하세요 Vue!'
-  }
-})
+        el: '#app',
+        data: {
+            message: '안녕하세요 Vue!'
+        }
+    })
 
     var app2 = new Vue({
-  el: '#app2',
-  data: {
-    message2: '안녕하세요 Vue2!'
-  }
-})
+        el: '#app2',
+        data: {
+            message2: '안녕하세요 Vue2!'
+        }
+    })
+
+    var vm = new Vue({
+        el: '#vue-instance',
+        data: data
+    })
+
+    var vm2 = new Vue({
+        el: '#another-instance',
+        data: data
+    })
+
 </script>
 </body>
 </html>
