c3428c07453fc6e3370b656fbbe3448b57c77ee9
Seungmin Lim <seungmin.lim@sk.com>
2017-11-21 20:10:01 +0900

    모든 센터 L4 설정 및 Mapping 조회 구현, timeout=2(상용에서 재조정 필요해보임)

--- l4ui/views.py
+++ l4ui/views.py
@@ -7,7 +7,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-L4_URL = ['http://i-php-iapi.skplanet.com/l4check/', 'http://c-php-iapi.skplanet.com/l4check/', 'http://b-php-iapi.skplanet.com/l4check/']
+L4_URL = ['http://c-php-iapi.skplanet.com/l4check/', 'http://b-php-iapi.skplanet.com/l4check/', 'http://i-php-iapi.skplanet.com/l4check/']
+#L4_URL = ['http://i-php-iapi.skplanet.com/l4check/']
 
 @login_required(login_url='/login')
 def create_l4(request):
@@ -16,43 +17,58 @@ def create_l4(request):
 
 @login_required(login_url='/login')
 def l4check(request):
-    BASE_URL = L4_URL[0] + 'check_json.php?'
-    r = requests.get(BASE_URL + 'ip=' + request.GET['ip'])
+    for url in L4_URL:
+        BASE_URL = url + 'check_json.php?'
+        try:
+            r = requests.get(BASE_URL + 'ip=' + request.GET['ip'], timeout=2)
+        except:
+            continue
 
-    # 비정상 IP 입력시 ${sIDC}-php-iapi의 결과는 "Please give me a valid IP Address\n"
-    if r.text[:-1] == "Please give me a valid IP Address":
-        return HttpResponse("정상적인 IP를 입력해주세요.")
+        # 비정상 IP 입력시 ${sIDC}-php-iapi의 결과는 "Please give me a valid IP Address\n"
+        if r.text[:-1] == "Please give me a valid IP Address":
+            continue
 
-    try:
-        vip_list = json.loads(r.text).keys()
-    except:
-        return HttpResponse("정상적인 IP를 입력해주세요.")
+        try:
+            vip_list = json.loads(r.text).keys()
+        except:
+            continue
 
-    if vip_list != []:
-        return HttpResponse(json.dumps({"data": "[WARNING] 해당 Virtual IP는 사용 불가능 합니다.", "status": "500"}))
-    else:
-        return HttpResponse(json.dumps({"data": "[OK] 해당 Virtual IP는 사용 가능 합니다.", "status": "200"}))
+        if vip_list != []:
+            return HttpResponse(json.dumps({"data": "[WARNING] 해당 Virtual IP는 사용 불가능 합니다.", "status": "404"}))
+        else:
+            return HttpResponse(json.dumps({"data": "[OK] 해당 Virtual IP는 사용 가능 합니다.", "status": "200"}))
+
+    return HttpResponse(json.dumps({"data": "정상적인 IP를 입력해주세요.", "status": "500"}))
 
 
+# 아래의 l4map 함수는 추후 API G/W에서 캐싱한 데이터를 사용하는 것이 좋아보임
 @login_required(login_url='/login')
 def l4map(request):
-    BASE_URL = L4_URL[0] + 'F5Map.txt'
-    l4map_list = []
+    for url in L4_URL:
+        BASE_URL = url + 'F5Map.txt'
+        l4map_list = []
 
-    r= requests.get(BASE_URL)
-    r_list = r.text.split('\n')
-    for el in r_list:
-        l4map_list.append(el.split('\t'))
+        try:
+            r= requests.get(BASE_URL, timeout=2)
+        except:
+            continue
+        try:
+            r_list = r.text.split('\n')
+            for el in r_list:
+                l4map_list.append(el.split('\t'))
 
-    selected_dev = ''
-    vip_range = '.'.join(request.GET['ip'].split('.')[:-1])
-    # 아래는 순차탐색이므로 추후 이진탐색으로 변경해야함
-    for l4_el in l4map_list:
-        if vip_range == '.'.join(l4_el[-2].split('.')[:-1]):
-            selected_dev = l4_el[0]
-            break
+            selected_dev = ''
+            vip_range = '.'.join(request.GET['ip'].split('.')[:-1])
+            # 아래는 순차탐색이므로 추후 이진탐색으로 변경해야함
+            for l4_el in l4map_list:
+                if vip_range == '.'.join(l4_el[-2].split('.')[:-1]):
+                    selected_dev = [l4_el[0], l4_el[1]]
+                    break
+            return HttpResponse(json.dumps(selected_dev))
+        except:
+            continue
 
-    return HttpResponse(selected_dev)
+    return HttpResponse("매칭되는 L4 장비가 없습니다.")
 
 
 @login_required(login_url='/login')
